0f17e55888e743ee65bb66c3ada69421c18cc199
Carlos-777-siuu <carlos.quintero4984@alumnos.udg.mx>
2025-12-15 16:52:37 -0600

    corrigiendo modelo usuarios

--- modelos/usuarios.py
+++ modelos/usuarios.py
@@ -1,18 +1,15 @@
 class Usuario:
-    def __init__(self, nombre, email, rol, contrasena, clientes=None, pagos=None):
+    def __init__(self,id, nombre, email, rol):
+        self.id = id
         self.nombre = nombre
         self.email = email
         self.rol = rol
-        self.contrasena = contrasena
-        self.clientes_registrados = clientes or []
-        self.pagos_registrados = pagos or []
-    
+
     def to_dict(self):
         return {
+                "id": self.id,
                 "nombre": self.nombre,
                 "email": self.email,
-                "rol": self.rol,
-                "clientes_registrados": self.clientes_registrados,
-                "pagos_registrados": self.pagos_registrados
+                "rol": self.rol
                 }
 
