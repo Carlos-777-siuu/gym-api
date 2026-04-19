be6615ccf73520930b1dd750fe173dd6fbbbf8c4
Carlos Quintero <carlos.quintero4984@alumnos.udg.mx>
2026-04-18 22:50:21 -0600

    Corrigiendo de el nombre de la clase que representa a las membresias de Membresias a Membresi porque cada instancia de clase representa a una sola entidad membresia

--- modelos/membresias.py
+++ modelos/membresias.py
@@ -1,17 +1,17 @@
-class Membresias:
-    def __init__(self,id,nombre,duracion_dias,precio):
+class Membresia:
+    def __init__(self,id,nombre_membresia,duracion_dias,precio_membresia):
         self.id = id
-        self.nombre_membresia = nombre
+        self.nombre_membresia = nombre_membresia
         self.duracion_dias = duracion_dias
-        self.precio_membresia = precio
+        self.precio_membresia = precio_membresia
 
     @classmethod
     def from_row(cls, row: dict):
         return cls(
             id= row["id"],
-            nombre_membresia=row["nombre_membresia"],
+            nombre_membresia=row["nombre"],
             duracion_dias = row["duracion_dias"],
-            precio_membresia = row["precio_membresia"]
+            precio_membresia = row["precio"]
         )
 
     def to_dict(self):
